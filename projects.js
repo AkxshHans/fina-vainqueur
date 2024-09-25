f8d8e663b9200d7409ea039831c9e4c99d5f646a
Akash Hans <162800417+AkxshHans@users.noreply.github.com>
2024-09-25 15:10:47 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -102,7 +102,7 @@ createProjects();
 
 const blogPosts = [
     {
-        title: '@vainqueuragency ',
+        title: '@vainqueur_agency ',
         time: 'INSTAGRAM',
         image: 'https://images.unsplash.com/photo-1633113247735-45a969eb9266?q=80&w=1537&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
